ed5b338668bbac0f668ded95b1dadd4ca8de7dd8
Uka0001 <ukraine0001@gmail.com>
2022-08-21 21:03:07 +0300

    some bug need fix in table class

--- src/main/java/org/uka0001/ORMInterface.java
+++ src/main/java/org/uka0001/ORMInterface.java
@@ -1,15 +1,12 @@
 package org.uka0001;
 
-import lombok.SneakyThrows;
 import org.uka0001.read_write_sourse.DataReadWriteSource;
 
 import java.util.List;
 
 public interface ORMInterface {
 
-    @SneakyThrows
     <T> List<T> readAll(DataReadWriteSource<?> source, Class<T> cls);
-
 
     default <T> void writeAll(DataReadWriteSource target, List<T> object) {
 
